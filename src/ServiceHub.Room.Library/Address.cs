c7526d668dfadd7732b7b23f24472dc752927d93
Daniel Hardy <danielhardy302@gmail.com>
2018-06-07 11:13:17 -0400

    added xml docs to models [#157252834]

--- src/ServiceHub.Room.Library/Address.cs
+++ src/ServiceHub.Room.Library/Address.cs
@@ -4,21 +4,23 @@ using System.Text;
 
 namespace ServiceHub.Room.Library
 {
+    //<summary> Address Model Referenced by the Room model. </summary>
     public class Address
 
     {
+        //<summary> Key. Used to uniquely identify this Address model. </summary>
         public Guid AddressId { get; set; }
-
+        //<summary> Steet number and street name of this address. </summary>
         public string Address1 { get; set; }
-
+        //<summary> Second line of the address, if you've ever needed such a thing. </summary>
         public string Address2 { get; set; }
-
+        //<summary> Name of the city for this address. </summary>
         public string City { get; set; }
-
+        //<summary> Name of the state for this address. </summary>
         public string State { get; set; }
-
+        //<summary> 5 to 9 digit number specifing region. </summary>
         public string PostalCode { get; set; }
-
+        //<summary> Name of the country for this address. </summary>
         public string Country { get; set; }
 
     }
--- src/ServiceHub.Room.Library/Room.cs
+++ src/ServiceHub.Room.Library/Room.cs
@@ -4,19 +4,25 @@ using System.Text;
 
 namespace ServiceHub.Room.Library
 {
+    //<summary> Room model. </summary>
     public class Room
 
     {
+        //<summary> Key. Used to uniquely identify this Room model. </summary>
         public Guid RoomId { get; set; }
-
+        //<summary> Name of the trainning location where this room is
+        //located.  Example: "Reston", "Tampa" or, "Dallas".
+        //</summary>
         public string Location { get; set; }
-
+        //<summary> This Address model for this room. </summary>
         public Address Address { get; set; }
-
+        //<summary> Number of unassigned beds in this room. </summary>
         public int Vacancy { get; set; }
-
+        //<summary> Total number of beds in this room weather assigned or not</summary>
         public int Occupancy { get; set; }
-
+        //<summary> Biological birth sex that this room accomidates.
+        // Usage: 'M' or 'F'. Mutually exclusive.
+        //</summary>
         public char Gender { get; set; }
 
     }
