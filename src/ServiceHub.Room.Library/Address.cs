a0dd77c782c9bab8cacd5d8104de6d35aca051c9
Daniel Hardy <danielhardy302@gmail.com>
2018-06-07 10:24:16 -0400

    added Room and Address Models to Library [#157252834]

---
+++ src/ServiceHub.Room.Library/Address.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceHub.Room.Library
+{
+    public class Address
+
+    {
+        public Guid AddressId { get; set; }
+
+        public string Address1 { get; set; }
+
+        public string Address2 { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string Country { get; set; }
+
+    }
+}
---
+++ src/ServiceHub.Room.Library/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceHub.Room.Library
+{
+    public class Room
+
+    {
+        public Guid RoomId { get; set; }
+
+        public string Location { get; set; }
+
+        public Address Address { get; set; }
+
+        public int Vacancy { get; set; }
+
+        public int Occupancy { get; set; }
+
+        public char Gender { get; set; }
+
+    }
+}
