288503e4fe8529fe4bf344516397603e06818b30
Ethan Misale <emisale94@gmail.com>
2018-06-09 18:42:21 -0400

    Added Repository and Context [#157252834]

---
+++ src/ServiceHub.Room.Context/Repository/IRoomRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServiceHub.Room.Context.Repository
+{
+    public interface IRoomsRepository
+    {
+
+        void Insert(Models.Room room);
+        IQueryable<Models.Room> Get();
+        Models.Room GetById(Guid id);
+        void Update(Models.Room room);
+        void Delete(Guid id);
+
+    }
+}
---
+++ src/ServiceHub.Room.Context/Repository/RoomsRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServiceHub.Room.Context.Repository
+{
+    public class RoomsRepository: IRoomsRepository
+    {
+        public void Insert(Models.Room room)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Models.Room> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Models.Room GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Models.Room room)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
