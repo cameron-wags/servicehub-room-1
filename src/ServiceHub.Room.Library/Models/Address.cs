5f9faf6969c4cf1bcb65dab2826abbc0cc424612
Daniel Hardy <danielhardy302@gmail.com>
2018-06-09 17:35:20 -0400

    added models and test

---
+++ src/ServiceHub.Room.Library/Models/Address.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ServiceHub.Room.Library
+{
+    //<summary> Address Model Referenced by the Room model. </summary>
+    public class Address
+
+    {
+        //<summary> Key. Used to uniquely identify this Address model. </summary>
+        public Guid AddressId { get; set; }
+        //<summary> Steet number and street name of this address. </summary>
+        public string Address1 { get; set; }
+        //<summary> Apt/Room number for this address if applicable. </summary>
+        public string Address2 { get; set; }
+        //<summary> Name of the city for this address. </summary>
+        public string City { get; set; }
+        //<summary> Name of the state for this address. </summary>
+        public string State { get; set; }
+        //<summary> 5 digit number specifing region. </summary>
+        public string PostalCode { get; set; }
+        //<summary> Name of the country for this address. </summary>
+        public string Country { get; set; }
+
+        public bool isValidState(Address model)
+        {
+            if (AddressId == System.Guid.Empty) { return false; }
+            if (Address1 == null || Address1.Length > 255) { return false; }
+            if (Address2 == null || Address2.Length > 255) { return false; }
+            if (City == null || City.Length > 25) { return false; }
+            if (State == null || State.Length > 2 || State.Length < 2) { return false; }
+            if (PostalCode == null || PostalCode.Length > 5 || PostalCode.Length < 5) { return false; }
+            if (Country == null || Country.Length > 3 || Country.Length < 3) { return false; }
+
+            return true;
+
+        }
+    }
+}
---
+++ src/ServiceHub.Room.Library/Models/Room.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ServiceHub.Room.Library
+{
+    //<summary> Room model. </summary>
+    public class Room
+    {
+        //<summary> Key. Used to uniquely identify this Room model. </summary>
+
+        public Guid RoomId { get; set; }
+        //<summary> Name of the training location where this room is
+        //located.  Example: "Reston", "Tampa" or, "Dallas".
+        //</summary>
+        [StringLength(255)]
+        public string Location { get; set; }
+        //<summary> This Address model for this room. </summary>
+        public Address Address { get; set; }
+        //<summary> Number of unassigned beds in this room. </summary>
+        public int? Vacancy { get; set; }
+        //<summary> Total number of beds in this room weather assigned or not</summary>
+        public int? Occupancy { get; set; }
+        //<summary> Sex that this room accommodates. </summary>\
+        public GenderEnum? Gender { get; set; }
+
+        public bool isValidState(Room model)
+        {
+            if (RoomId == System.Guid.Empty) { return false; }
+            if (Location == null || Location.Length > 255) { return false; }
+            if (Address == null || !Address.isValidState(this.Address)) { return false; }
+            if (Vacancy == null || Vacancy > Occupancy) { return false; }
+            if (Occupancy == null || Occupancy <= 0) { return false; }
+            if (Gender == null) { return false; }
+
+            return true;
+
+        }
+    }
+
+    [DataContract(Name = "Gender")]
+    public enum GenderEnum
+    {
+        [EnumMember]
+        Male,
+        [EnumMember]
+        Female
+
+
+    }
+}
