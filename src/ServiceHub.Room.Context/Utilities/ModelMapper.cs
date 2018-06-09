8bd06c38e7d73d78bf6e011c78c99eacb25f8c8f
Unknown <cameron.wagstaff.129@gmail.com>
2018-06-09 19:30:47 -0400

    created model mapper [#157252834]

---
+++ src/ServiceHub.Room.Context/Utilities/ModelMapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceHub.Room.Context.Utilities
+{
+    public static class ModelMapper
+    {
+        public static Models.Room LibraryToContext(Library.Room libraryRoom) {
+            throw new NotImplementedException();
+        }
+
+        public static Library.Room ContextToLibrary(Models.Room contextRoom) {
+            throw new NotImplementedException();
+        }
+    }
+}
