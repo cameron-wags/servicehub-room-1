7021439332357466e503062e41d65815b4b635b6
Cameron Wagstaff <cameron.wagstaff.129@gmail.com>
2018-06-13 12:30:57 -0400

    Changing genders so that there is only one valid representation of each gender.
    Also added a test but who cares.

--- src/ServiceHub.Room.Testing/Context/ModelMapperTest.cs
+++ src/ServiceHub.Room.Testing/Context/ModelMapperTest.cs
@@ -1,9 +1,12 @@
-﻿using ServiceHub.Room.Context.Utilities;
+﻿using System;
+using ServiceHub.Room.Context.Utilities;
+using ServiceHub.Room.Library.Models;
 using Xunit;
 
 namespace ServiceHub.Room.Testing.Context {
     
     public class LibToContextRoomTests {
+
         [Fact]
         public void NullParameter() {
             var actual = ModelMapper.LibraryToContext(null);
@@ -13,7 +16,35 @@ namespace ServiceHub.Room.Testing.Context {
 
         [Fact]
         public void InvalidModelInput() {
+            var invalidModel = new Room.Library.Models.Room();
 
+            var actual = ModelMapper.LibraryToContext(invalidModel);
+
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        public void ValidModelInput() {
+            var validModel = new Room.Library.Models.Room() {
+                RoomId = new Guid(),
+                Location = "Tampa",
+                Address = new Address() {
+                    AddressId = new Guid(),
+                    Address1 = "123 Street Ln",
+                    Address2 = "",
+                    City = "Lutz",
+                    State = "FL",
+                    PostalCode = "33412",
+                    Country = "US"
+                },
+                Vacancy = 4,
+                Occupancy = 6,
+                Gender = "M"
+            };
+
+            var actual = ModelMapper.LibraryToContext(validModel);
+
+            Assert.Equal(validModel.ToString(), actual.ToString());
         }
     }
 
