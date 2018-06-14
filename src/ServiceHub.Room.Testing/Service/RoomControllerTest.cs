296c82009559ed5489e5741e4335ad148506941a
Daniel Hardy <danielhardy302@gmail.com>
2018-06-14 15:04:48 -0400

    add some test for controller

--- src/ServiceHub.Room.Testing/Service/RoomControllerTest.cs
+++ src/ServiceHub.Room.Testing/Service/RoomControllerTest.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Web.Http.Results;
-using MongoDB.Bson;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using ServiceHub.Room.Context.Models;
 using ServiceHub.Room.Service.Controllers;
 using ServiceHub.Room.Context.Repository;
@@ -42,7 +43,7 @@ namespace ServiceHub.Room.Testing.Service
         }
 
         [Fact]
-        public void TestControllerGet()
+        public async void TestControllerGet()
         {
             //Arrange
             room.Address = address;
@@ -51,11 +52,35 @@ namespace ServiceHub.Room.Testing.Service
             RoomController roomController = new RoomController(null,null,context);
 
             //Act
-            var response = roomController.Get();
+            //var response = await roomController.Get();
+            var myTask = Task.Run(() => roomController.Get());
+            var result = await myTask;
+            var contentResult = result as OkObjectResult;
 
-            var contentResult = response.Result.ToJson();
-            Assert.True(response.IsCompletedSuccessfully);
-            
+
+            var list = contentResult.Value as List<Room.Library.Models.Room>;
+            Guid id1 = list[0].RoomId;
+
+            Assert.Equal(id,id1);
+
+        }
+
+        [Fact]
+        public async void TestControllerGetById()
+        {
+            //Arrange
+            room.Address = address;
+            Guid id = room.RoomId;
+            context.Insert(room);
+            RoomController roomController = new RoomController(null, null, context);
+
+            var myTask = Task.Run(() => roomController.Get(id));
+            var result = await myTask;
+            var contentResult = result as OkObjectResult;
+
+            var roomReturned = contentResult.Value as Room.Library.Models.Room;
+
+            Assert.Equal(id,roomReturned.RoomId);
         }
     }
 }
