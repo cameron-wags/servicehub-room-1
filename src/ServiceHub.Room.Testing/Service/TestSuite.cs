f237fcbe6cea8c9f9082bc73bf890cd982e0a59e
Daniel Hardy <danielhardy302@gmail.com>
2018-06-14 09:53:33 -0400

    compare 2 objects usingToJson()

--- src/ServiceHub.Room.Testing/Service/TestSuite.cs
+++ src/ServiceHub.Room.Testing/Service/TestSuite.cs
@@ -1,15 +1,68 @@
 using System;
 using System.Collections.Generic;
+using MongoDB.Bson;
 using Xunit;
 using ServiceHub.Room.Context;
 using ServiceHub.Room.Context.Repository;
 using ServiceHub.Room.Context.Models;
+using Newtonsoft.Json;
 
 namespace ServiceHub.Room.Testing.Service
 {
     public class TestSuite
     {
         private RoomRepositoryMemory context;
+        private Address address;
+        private Room.Context.Models.Room room;
+        public TestSuite()
+        {
+            address = address = new Address
+            {
+                AddressId = Guid.NewGuid(),
+                Address1 = "1234 Test st.",
+                Address2 = "apt 303",
+                City = "Tampa",
+                Country = "US",
+                PostalCode = "92646",
+                State = "Ca"
+            };
+
+            room = new Room.Context.Models.Room
+            {
+                RoomId = Guid.NewGuid(),
+                Address = address,
+                Location = "Tampa",
+                Vacancy = 1,
+                Occupancy = 2,
+                Gender = "M"
+            };
+        }
+
+        [Fact]
+        public void TestModelProperties()
+        {
+            Address address1 = new Address();
+            address1.AddressId = address.AddressId;
+            address1.Address1 = address1.Address1;
+            address1.Address2 = address.Address2;
+            address1.City = address.City;
+            address1.State = address1.State;
+            address1.Country = address.Country;
+            address1.PostalCode = address.PostalCode;
+
+            Room.Context.Models.Room room1 = new Room.Context.Models.Room();
+            room1.RoomId = room.RoomId;
+            room1.Address = room.Address;
+            room1.Location = room.Location;
+            room1.Occupancy = room.Occupancy;
+            room1.Vacancy = room.Vacancy;
+            room1.Gender = room.Gender;
+
+           
+            var obj1ToJSON = room.ToJson();
+            var obj2ToJSON = room.ToJson();
+            Assert.Equal(obj1ToJSON,obj2ToJSON);
+        }
 
         [Fact]
         public void TestContextInsert()
