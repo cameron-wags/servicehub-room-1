8fac5a3b391c9998c01d8223037c5aaf97ac72fa
Cameron Wagstaff <cameron.wagstaff.129@gmail.com>
2018-06-13 22:02:57 -0400

    Model mapper tests done

--- src/ServiceHub.Room.Testing/Context/ModelMapperTest.cs
+++ src/ServiceHub.Room.Testing/Context/ModelMapperTest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using MongoDB.Bson;
 using ServiceHub.Room.Context.Utilities;
 using ServiceHub.Room.Library.Models;
 using Xunit;
@@ -27,12 +29,84 @@ namespace ServiceHub.Room.Testing.Context {
         }
 
         [Fact]
-        public void ValidModelInput() {
-            var validModel = new Room.Library.Models.Room() {
-                RoomId = new Guid(),
+        public void NulledModel() {
+            var nulledModel = new Room.Library.Models.Room() {
+                RoomId = Guid.Empty,
+                Location = null,
+                Address = new Address() {
+                    AddressId = Guid.Empty,
+                    Address1 = null,
+                    Address2 = null,
+                    City = null,
+                    State = null,
+                    PostalCode = null,
+                    Country = null
+                },
+                Vacancy = null,
+                Occupancy = null,
+                Gender = null
+            };
+
+            var actual = ModelMapper.LibraryToContext(nulledModel);
+
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        public void InvalidModelInput() {
+            var invalidModel = new Room.Library.Models.Room() {
+                RoomId = Guid.NewGuid(),
                 Location = "Tampa",
                 Address = new Address() {
-                    AddressId = new Guid(),
+                    AddressId = Guid.NewGuid(),
+                    Address1 = "123 Street Ln",
+                    Address2 = "",
+                    City = "Lutz",
+                    State = "FL",
+                    PostalCode = "33412",
+                    Country = "US"
+                },
+                Vacancy = -4,
+                Occupancy = 6,
+                Gender = "M"
+            };
+
+            var actual = ModelMapper.LibraryToContext(invalidModel);
+
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        public void IntsAreNull() {
+            var nulledInts = new Room.Library.Models.Room() {
+                RoomId = Guid.NewGuid(),
+                Location = "Tampa",
+                Address = new Address() {
+                    AddressId = Guid.NewGuid(),
+                    Address1 = "123 Street Ln",
+                    Address2 = "",
+                    City = "Lutz",
+                    State = "FL",
+                    PostalCode = "33412",
+                    Country = "US"
+                },
+                Vacancy = null,
+                Occupancy = null,
+                Gender = "M"
+            };
+
+            var actual = ModelMapper.LibraryToContext(nulledInts);
+
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        public void ValidModelInput() {
+            var validModel = new Room.Library.Models.Room() {
+                RoomId = Guid.NewGuid(),
+                Location = "Tampa",
+                Address = new Address() {
+                    AddressId = Guid.NewGuid(),
                     Address1 = "123 Street Ln",
                     Address2 = "",
                     City = "Lutz",
@@ -47,11 +121,23 @@ namespace ServiceHub.Room.Testing.Context {
 
             var actual = ModelMapper.LibraryToContext(validModel);
 
-            Assert.Equal(validModel.ToString(), actual.ToString());
+            Assert.Equal(validModel.RoomId, actual.RoomId);
+            Assert.Equal(validModel.Location, actual.Location);
+            Assert.Equal(validModel.Address.AddressId, actual.Address.AddressId);
+            Assert.Equal(validModel.Address.Address1, actual.Address.Address1);
+            Assert.Equal(validModel.Address.Address2, actual.Address.Address2);
+            Assert.Equal(validModel.Address.City, actual.Address.City);
+            Assert.Equal(validModel.Address.State, actual.Address.State);
+            Assert.Equal(validModel.Address.PostalCode, actual.Address.PostalCode);
+            Assert.Equal(validModel.Address.Country, actual.Address.Country);
+            Assert.Equal(validModel.Vacancy, actual.Vacancy);
+            Assert.Equal(validModel.Occupancy, actual.Occupancy);
+            Assert.Equal(validModel.Gender, actual.Gender);
         }
     }
-
+    
     public class RoomContextToLibTests {
+
         [Fact]
         public void NullParameter() {
             Room.Context.Models.Room nullRoom = null;
@@ -71,11 +157,36 @@ namespace ServiceHub.Room.Testing.Context {
         }
 
         [Fact]
-        public void ValidModelInput() {
-            var validModel = new Room.Context.Models.Room() {RoomId = new Guid(),
+        public void InvalidModelInput() {
+            var invalidModel = new Room.Context.Models.Room() {
+                RoomId = Guid.NewGuid(),
                 Location = "Tampa",
                 Address = new Room.Context.Models.Address() {
-                    AddressId = new Guid(),
+                    AddressId = Guid.NewGuid(),
+                    Address1 = "123 Street Ln",
+                    Address2 = "",
+                    City = "Lutz",
+                    State = "FL",
+                    PostalCode = "33412",
+                    Country = "US"
+                },
+                Vacancy = -4,
+                Occupancy = 6,
+                Gender = "M"
+            };
+
+            var actual = ModelMapper.ContextToLibrary(invalidModel);
+
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        public void ValidModelInput() {
+            var validModel = new Room.Context.Models.Room() {
+                RoomId = Guid.NewGuid(),
+                Location = "Tampa",
+                Address = new Room.Context.Models.Address() {
+                    AddressId = Guid.NewGuid(),
                     Address1 = "123 Street Ln",
                     Address2 = "",
                     City = "Lutz",
@@ -90,11 +201,23 @@ namespace ServiceHub.Room.Testing.Context {
 
             var actual = ModelMapper.ContextToLibrary(validModel);
 
-            Assert.Equal(validModel.ToString(), actual.ToString());
+            Assert.Equal(validModel.RoomId, actual.RoomId);
+            Assert.Equal(validModel.Location, actual.Location);
+            Assert.Equal(validModel.Address.AddressId, actual.Address.AddressId);
+            Assert.Equal(validModel.Address.Address1, actual.Address.Address1);
+            Assert.Equal(validModel.Address.Address2, actual.Address.Address2);
+            Assert.Equal(validModel.Address.City, actual.Address.City);
+            Assert.Equal(validModel.Address.State, actual.Address.State);
+            Assert.Equal(validModel.Address.PostalCode, actual.Address.PostalCode);
+            Assert.Equal(validModel.Address.Country, actual.Address.Country);
+            Assert.Equal(validModel.Vacancy, actual.Vacancy);
+            Assert.Equal(validModel.Occupancy, actual.Occupancy);
+            Assert.Equal(validModel.Gender, actual.Gender);
         }
     }
 
     public class RoomListContextToLibTests {
+
         [Fact]
         public void NullParameter() {
             List<Room.Context.Models.Room> nullList = null;
@@ -112,19 +235,93 @@ namespace ServiceHub.Room.Testing.Context {
 
             Assert.Empty(actual);
         }
-    }
 
-    public class AddressLibToContextTests {
         [Fact]
-        public void NullParameter() {
+        public void ContainsInvalid() {
+            var ctxRooms = new List<Room.Context.Models.Room>() {
+                new Room.Context.Models.Room() {
+                    RoomId = Guid.NewGuid(),
+                    Location = "Tampa",
+                    Address = new Room.Context.Models.Address() {
+                        AddressId = Guid.NewGuid(),
+                        Address1 = "123 Street Ln",
+                        Address2 = "apt 1",
+                        City = "Lutz",
+                        State = "FL",
+                        PostalCode = "33412",
+                        Country = null
+                    },
+                    Vacancy = 4,
+                    Occupancy = 6,
+                    Gender = "M"
+                },
+                new Room.Context.Models.Room() {
+                    RoomId = Guid.NewGuid(),
+                    Location = "Tampa",
+                    Address = new Room.Context.Models.Address() {
+                        AddressId = Guid.NewGuid(),
+                        Address1 = "123 Street Ln",
+                        Address2 = "apt 2",
+                        City = "Lutz",
+                        State = "FL",
+                        PostalCode = "33412",
+                        Country = "US"
+                    },
+                    Vacancy = 4,
+                    Occupancy = 6,
+                    Gender = "F"
+                }
+            };
 
+            var actual = ModelMapper.ContextToLibrary(ctxRooms);
+
+            Assert.Null(actual);
         }
-    }
 
-    public class AddressContextToLibTests {
         [Fact]
-        public void NullParameter() {
+        public void ValidModels() {
+            var ctxRooms = new List<Room.Context.Models.Room>() {
+                new Room.Context.Models.Room() {
+                    RoomId = Guid.NewGuid(),
+                    Location = "Tampa",
+                    Address = new Room.Context.Models.Address() {
+                        AddressId = Guid.NewGuid(),
+                        Address1 = "123 Street Ln",
+                        Address2 = "apt 1",
+                        City = "Lutz",
+                        State = "FL",
+                        PostalCode = "33412",
+                        Country = "US"
+                    },
+                    Vacancy = 4,
+                    Occupancy = 6,
+                    Gender = "M"
+                },
+                new Room.Context.Models.Room() {
+                    RoomId = Guid.NewGuid(),
+                    Location = "Tampa",
+                    Address = new Room.Context.Models.Address() {
+                        AddressId = Guid.NewGuid(),
+                        Address1 = "123 Street Ln",
+                        Address2 = "apt 2",
+                        City = "Lutz",
+                        State = "FL",
+                        PostalCode = "33412",
+                        Country = "US"
+                    },
+                    Vacancy = 4,
+                    Occupancy = 6,
+                    Gender = "F"
+                }
+            };
 
+            var libRoom1 = ModelMapper.ContextToLibrary(ctxRooms[0]);
+            var libRoom2 = ModelMapper.ContextToLibrary(ctxRooms[1]);
+
+            var actual = ModelMapper.ContextToLibrary(ctxRooms);
+            
+            Assert.Contains(libRoom1.ToJson(), actual.Select(r => r.ToJson()));
+            Assert.Contains(libRoom2.ToJson(), actual.Select(r => r.ToJson()));
         }
     }
 }
