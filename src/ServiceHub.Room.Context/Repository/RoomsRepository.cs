1dbe4a281f4e010902cac7faad14787cc33f9099
Ethan Misale <emisale94@gmail.com>
2018-06-15 11:17:27 -0400

    Documented all context code

--- src/ServiceHub.Room.Context/Repository/RoomsRepository.cs
+++ src/ServiceHub.Room.Context/Repository/RoomsRepository.cs
@@ -13,11 +13,21 @@ namespace ServiceHub.Room.Context.Repository
     public class RoomsRepository: IRoomsRepository
     {
         private readonly IMongoCollection<Models.Room> _collection;
-     
+
+        /// <summary>
+        /// Instantiates the IMongoCollection and sets the readonly _collection property
+        /// to the returned Mongo Collection object.
+        /// </summary>
+        /// <param name="collection"></param>
         public RoomsRepository(IMongoCollection<Models.Room> collection)
         {
             _collection = collection;
         }
+
+        /// <summary>
+        /// Creates a room within the MongoDatabase. If the room is null an ArgumentNullException is thrown.
+        /// </summary>
+        /// <param name="room"></param>
         public void Insert(Models.Room room)
         {
             if (room == null)
@@ -27,6 +37,10 @@ namespace ServiceHub.Room.Context.Repository
             _collection.InsertOne(room);
         }
 
+        /// <summary>
+        /// Gets all rooms from the Mongo database.
+        /// </summary>
+        /// <returns>Returns a list of all the Models.Room within the database. If no rooms are found it returns an ArgumentNullException.</returns>
         public List<Models.Room> Get()
         {
             if (_collection == null)
@@ -38,6 +52,11 @@ namespace ServiceHub.Room.Context.Repository
 
         }
 
+        /// <summary>
+        /// Gets all rooms from the Mongo database based on the Guid passed in.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Returns a Models.Room object from the database. Returns an ArgumentNullException is the room exists based on the id.</returns>
         public Models.Room GetById(Guid id)
         {
             if (id == null)
@@ -49,6 +68,12 @@ namespace ServiceHub.Room.Context.Repository
 
         }
 
+        /// <summary>
+        /// Updates a Models.Room object in the database based on the RoomId.
+        /// If a room object is not passed in an ArgumentNullException is thrown.
+        /// If no room object corresponds with the passed in room, no room is updated.
+        /// </summary>
+        /// <param name="room"></param>
         public void Update(Models.Room room)
         {
             if (room == null)
@@ -59,6 +84,11 @@ namespace ServiceHub.Room.Context.Repository
             _collection.FindOneAndReplace(x => x.RoomId == room.RoomId, room);
         }
 
+        /// <summary>
+        /// Deletes a Models.Room object in the database based on the RoomId.
+        /// If the RoomId is not found an ArgumentNullException is thrown.
+        /// </summary>
+        /// <param name="id"></param>
         public void Delete(Guid id)
         {
             if (id == null)
